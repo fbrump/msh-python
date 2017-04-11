7fbab2cb7d56f1295c40f4a9bbe8dd80b7b86531
Felipe Brum <fbrump@gmail.com>
2017-04-11 00:24:20 -0300

    Create unittest for the system with multiples situations that can happened in process

--- release/models.py
+++ release/models.py
@@ -8,65 +8,68 @@ from django.db import models
 from pointsheet.models import Pointsheet
 
 class Release(models.Model):
-    """
-    Description: That class mapping release of system.
+	"""
+	Description: That class mapping release of system.
 
-    Properties:
-    	date 			 		-- Date of release
-    	dayweek 		 		-- Day of week of release
-    	checkin 		 		-- Time of check-in job
-    	checkout_lunch 	 		-- Time of check-out to lunch
-    	checkin_lunch 	 		-- Time of check-in from lunch
-    	checkout 		 		-- Time of check-out job
-    	is_holiday 		 		-- Is holiday or not
-    	type_absence 	 		-- Type of absence, if exist
-    	checkin_absence  		-- Time of check-in to absence, if exist
-    	checkout_absence 		-- Time of check-out from absence, if exist
-    	justification_absence 	-- Description of justification of absence, if exist
-    	file_link 				-- File for link with document or something like this for justification absence, if existe
-    	pointsheet 				-- Pointsheet associated
-    """
-    SUNDAY = 'SUN'
-    MONDAY = 'MON'
-    TUESDAY = 'TUE'
-    WEDNESDAY = 'WED'
-    THUSDAY = 'THU'
-    FRIDAY = 'FRI'
-    SATURDAY = 'SAT'
-    DAY_OF_WEEK_CHOICES = (
-    	(SUNDAY, 'Sunday'),
-    	(MONDAY, 'Monday'),
-    	(TUESDAY, 'Tuesday'),
-    	(WEDNESDAY, 'Wednesday'),
-    	(THUSDAY, 'Thusday'),
-    	(FRIDAY, 'Friday'),
-    	(SATURDAY, 'Saturday'),
+	Properties:
+		date 			 		-- Date of release
+		dayweek 		 		-- Day of week of release
+		checkin 		 		-- Time of check-in job
+		checkout_lunch 	 		-- Time of check-out to lunch
+		checkin_lunch 	 		-- Time of check-in from lunch
+		checkout 		 		-- Time of check-out job
+		is_holiday 		 		-- Is holiday or not
+		type_absence 	 		-- Type of absence, if exist
+		checkin_absence  		-- Time of check-in to absence, if exist
+		checkout_absence 		-- Time of check-out from absence, if exist
+		justification_absence 	-- Description of justification of absence, if exist
+		file_link 				-- File for link with document or something like this for justification absence, if existe
+		pointsheet 				-- Pointsheet associated
+	"""
+	SUNDAY = 'SUN'
+	MONDAY = 'MON'
+	TUESDAY = 'TUE'
+	WEDNESDAY = 'WED'
+	THUSDAY = 'THU'
+	FRIDAY = 'FRI'
+	SATURDAY = 'SAT'
+	DAY_OF_WEEK_CHOICES = (
+		(SUNDAY, 'Sunday'),
+		(MONDAY, 'Monday'),
+		(TUESDAY, 'Tuesday'),
+		(WEDNESDAY, 'Wednesday'),
+		(THUSDAY, 'Thusday'),
+		(FRIDAY, 'Friday'),
+		(SATURDAY, 'Saturday'),
 	)
 	TYPE_ABSENCE_CHOICES = (
 		('1', 'Medical appointment'),
 		('2', 'Personal matters'),
-		('3', 'Day off'),)
-    date = models.DateField()
-    dayweek = models.CharField(
-    	max_length=3,
-    	choices=DAY_OF_WEEK_CHOICES
+		('3', 'Day off'),
 	)
-    checkin = models.TimeField()
-    checkout_lunch = models.TimeField(null=True)
-    checkin_lunch = models.TimeField(null=True)
-    checkout = models.TimeField()
-    is_holiday = models.BooleanField(default=False)
-    type_absence = models.CharField(
-    	max_length=3,
-    	choices=TYPE_ABSENCE_CHOICES
+	date = models.DateField()
+	dayweek = models.CharField(
+		max_length=3,
+		choices=DAY_OF_WEEK_CHOICES
 	)
-    checkin_absence = models.TimeField(null=True)
-    checkout_absence = models.TimeField(null=True)
-    justification_absence = models.TextField(null=True, max_length=5000)
-    file_link = models.URLField(max_length=2000, null=True)
-    # Relationsheep
-    pointsheet = models.ForeignKey(Pointsheet, on_delete=models.CASCADE)
+	checkin = models.TimeField()
+	checkout_lunch = models.TimeField(null=True)
+	checkin_lunch = models.TimeField(null=True)
+	checkout = models.TimeField()
+	is_holiday = models.BooleanField(default=False)
+	type_absence = models.CharField(
+		blank=True, 
+		null=True,
+		max_length=3,
+		choices=TYPE_ABSENCE_CHOICES
+	)
+	checkin_absence = models.TimeField(null=True)
+	checkout_absence = models.TimeField(null=True)
+	justification_absence = models.TextField(null=True, max_length=5000)
+	file_link = models.URLField(max_length=2000, null=True)
+	# Relationsheep
+	pointsheet = models.ForeignKey(Pointsheet, on_delete=models.CASCADE)
 
-    class Meta:
-        unique_together = ('date', 'pointsheet')
-        index_together = ['date', 'day_of_week', 'pointsheet']
+	class Meta:
+		unique_together = ('date', 'pointsheet')
+		index_together = ['date', 'dayweek', 'pointsheet']
--- release/tests.py
+++ release/tests.py
@@ -4,9 +4,100 @@
 from django.test import TestCase
 
 import unittest
+import datetime
 
+from pointsheet.models import Pointsheet
 from .models import Release
 
 class ReleaseTest(unittest.TestCase):
 	"""docstring for ReleaseTest"""
-	pass
+	def create_pointsheet_valid(self):
+		return Pointsheet.objects.create(year=2016, month=12)
+	def create_basic_release_normal(self, date, dayofweek, pointsheet):
+		return Release.objects.create(
+			date=date,
+			dayweek=dayofweek,
+			checkin= datetime.time(9, 0),
+			checkout_lunch= datetime.time(12, 0),
+			checkin_lunch= datetime.time(13, 0),
+			checkout= datetime.time(18, 0),
+			pointsheet=pointsheet
+		)
+	def create_basic_release_weeekend_work_without_lunch(self, date, dayofweek, pointsheet):
+		return Release.objects.create(
+			date=date,
+			dayweek=dayofweek,
+			checkin= datetime.time(9, 0),
+			checkout= datetime.time(14, 0),
+			pointsheet=pointsheet
+		)
+	def create_basic_release_weeekend_work(self, date, dayofweek, pointsheet):
+		return Release.objects.create(
+			date=date,
+			dayweek=dayofweek,
+			checkin= datetime.time(9, 0),
+			checkout_lunch= datetime.time(12, 0),
+			checkin_lunch= datetime.time(13, 0),
+			checkout= datetime.time(16, 0),
+			pointsheet=pointsheet
+		)
+	
+	def test_create_new_release(self):
+		"""
+			Description -- : Teste if create new release is correct or not
+			Senario ------ : One pointsheet for one release
+		"""
+		# Arrange
+		pointsheet = self.create_pointsheet_valid()
+		self.create_basic_release_normal(datetime.date(2016, 12, 01), Release.TUESDAY, pointsheet)
+		#Act
+		_count = len(Release.objects.all())
+		#Assert
+		self.assertEqual(_count, 1)
+
+	def test_create_new_release_multiples(self):
+		"""
+			Description -- : Teste if create new release is correct or not when have multiples inserts and work on weekends
+			Senario ------ : One pointsheet for multiples releases
+		"""
+		# Arrange
+		pointsheet = self.create_pointsheet_valid()
+		
+		self.create_basic_release_normal(datetime.date(2016, 12, 2), Release.WEDNESDAY, pointsheet)
+		self.create_basic_release_normal(datetime.date(2016, 12, 3), Release.FRIDAY, pointsheet)
+		self.create_basic_release_normal(datetime.date(2016, 12, 6), Release.MONDAY, pointsheet)
+		self.create_basic_release_normal(datetime.date(2016, 12, 7), Release.TUESDAY, pointsheet)
+		self.create_basic_release_normal(datetime.date(2016, 12, 8), Release.WEDNESDAY, pointsheet)
+		self.create_basic_release_normal(datetime.date(2016, 12, 9), Release.THUSDAY, pointsheet)
+		#Act
+		_count = len(Release.objects.all())
+		_release = Release.objects.get(id=2)
+		#Assert
+		self.assertEqual(_count, 7)
+		self.assertEqual(_release.dayweek, Release.WEDNESDAY)
+
+	def test_create_new_release_multiples_with_weekend_work(self):
+		"""
+			Description -- : Teste if create new release is correct or not when have multiples inserts and work on weekends
+			Senario ------ : One pointsheet for multiples releases (many differents)
+		"""
+		# Arrange
+		pointsheet = self.create_pointsheet_valid()
+
+		self.create_basic_release_weeekend_work_without_lunch(datetime.date(2016, 12, 10), Release.SATURDAY, pointsheet)
+		self.create_basic_release_normal(datetime.date(2016, 12, 12), Release.MONDAY, pointsheet)
+		self.create_basic_release_normal(datetime.date(2016, 12, 13), Release.TUESDAY, pointsheet)
+		self.create_basic_release_normal(datetime.date(2016, 12, 14), Release.WEDNESDAY, pointsheet)
+		self.create_basic_release_normal(datetime.date(2016, 12, 15), Release.THUSDAY, pointsheet)
+		self.create_basic_release_normal(datetime.date(2016, 12, 16), Release.FRIDAY, pointsheet)
+		self.create_basic_release_weeekend_work(datetime.date(2016, 12, 17), Release.SATURDAY, pointsheet)
+		self.create_basic_release_weeekend_work_without_lunch(datetime.date(2016, 12, 18), Release.SUNDAY, pointsheet)
+		self.create_basic_release_normal(datetime.date(2016, 12, 19), Release.MONDAY, pointsheet)
+		#Act
+		_count = len(Release.objects.all())
+		_release_date = Release.objects.get(date=datetime.date(2016, 12, 16))
+		_count_release_dayweek = len(Release.objects.filter(dayweek=Release.SATURDAY))
+		#Assert
+		self.assertEqual(_count, 16)
+		self.assertEqual(_release_date.dayweek, Release.FRIDAY)
+		self.assertEqual(_count_release_dayweek, 2)
