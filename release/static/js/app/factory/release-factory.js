9ba9a95aae7dcee4b6f1d49cd5202e7d389f1d3b
Felipe Brum <fbrump@gmail.com>
2017-04-13 23:51:55 -0300

    Create HTML for model and build HTML list and fixed structure for AngularJS

---
+++ release/static/js/app/factory/release-factory.js
@@ -0,0 +1,25 @@
+var myFactoryModule = angular.module('MyFactories', []);
+
+myFactoryModule.factory('ReleaseFactory', [function () {
+
+	var _ConstructorModel = function (form) {
+		return {
+			date: form['date'],
+			dayweek: form['dayweek'],
+			checkin: form['checkin'],
+			checkout_lunch: form['checkout_lunch'],
+			checkin_lunch: form['checkin_lunch'],
+			checkout: form['checkout'],
+			is_holiday: form['is_holiday'],
+			type_absence: form['type_absence'],
+			checkin_absence: form['checkin_absence'],
+			checkout_absence: form['checkout_absence'],
+			justification_absence: form['justification_absence'],
+			file_link: form['file_link'],
+			pointsheet: form['pointsheet']
+		};
+	}
+	return {
+		ConstructorModel: _ConstructorModel
+	};
+}]);
--- release/urls.py
+++ release/urls.py
@@ -9,8 +9,6 @@ from . import views
 from release.api import views as api_views
 
 urlpatterns = [
-	# url(r'^api/releases/$', views.PointsheetList.as_view(), name='pointsheets'),
-	# url(r'^api/releases/(?P<pk>[0-9]+)/$', views.PointsheetDetail.as_view(), name='pointsheets-detail'),
 	url(r'^api/releases/$', api_views.ReleaseList),
     url(r'^api/releases/(?P<pk>[0-9]+)/$', api_views.ReleaseDetail),
 	url(r'^release/$', views.ReleaseView.as_view(), name='release'),
